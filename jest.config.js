7e727757c4b29ce8173d941f2059d6fd50cd03ae
Olivia-Shch <165393515+Olivia-Shch@users.noreply.github.com>
2025-03-15 22:28:47 +1000

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 export default {
   transform: {
-    "^.+\\.js$": "babel-jest"
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest"
   },
   testEnvironment: "node",
   globals: {
     "ts-jest": {
       useESM: true
     }
-  }
+  },
+  setupFiles: ["./jest.setup.js"], // Если нужно
+  verbose: true, // Для более детализированных логов
 };
