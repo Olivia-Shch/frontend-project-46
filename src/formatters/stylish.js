063729bf0319c6b9d8359f7383ac8e8495b569a6
Olivia-Shch <165393515+Olivia-Shch@users.noreply.github.com>
2025-03-15 22:05:42 +1000

    Update stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,24 @@
 import _ from 'lodash';
 
-const addMargin = (depth, fullMargin = false) => {
-  if (fullMargin) {
-    return _.repeat(' ', depth * 4 - 2);
-  }
-  return _.repeat(' ', depth * 4 - 0);
+const addMargin = (depth, isSignMargin = false) => {
+  const baseIndent = ' '.repeat(depth * 4);
+  return isSignMargin ? baseIndent.slice(2) : baseIndent;
 };
 
 const makeString = (item, depth) => {
   if (!_.isObject(item) || item === null) {
     return String(item);
   }
-  const string = Object.entries(item).map(([key, value]) => ` ${addMargin(depth + 1, true)} ${key}: ${makeString(value, depth + 1)}`);
+  const string = Object.entries(item).map(([key, value]) => 
+    `${addMargin(depth + 1, true)} ${key}: ${makeString(value, depth + 1)}`
+  );
   return `{\n${string.join('\n')}\n${addMargin(depth)}}`;
 };
 
 const makeTree = (comparedData, depth = 1) => {
   const data = comparedData.map((item) => {
     if (item.type === 'nested') {
-      return `${addMargin(depth)}${item.key}: ${makeTree(item.children, depth + 1)}`;
+      return `${addMargin(depth)}${item.key}: ${makeString(makeTree(item.children, depth + 1), depth)}`;
     }
     if (item.type === 'deleted') {
       return `${addMargin(depth, true)}- ${item.key}: ${makeString(item.value1, depth)}`;
@@ -32,7 +32,7 @@ const makeTree = (comparedData, depth = 1) => {
       return `${string1}\n${string2}`;
     }
     if (item.type === 'same') {
-      return `${addMargin(depth)}${item.key}: ${makeString(item.value)}`;
+      return `${addMargin(depth)}${item.key}: ${makeString(item.value, depth)}`;
     }
     return item;
   });
@@ -42,3 +42,4 @@ const makeTree = (comparedData, depth = 1) => {
 const makeStylishReportDiff = (comparedData) => makeTree(comparedData);
 
 export default makeStylishReportDiff;
+
