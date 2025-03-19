6cfd37bf7f754ae3135ac423abe27a1029501b7c
Olivia-Shch <165393515+Olivia-Shch@users.noreply.github.com>
2025-03-19 17:01:49 +1000

    Update compare.js

--- src/compare.js
+++ src/compare.js
@@ -1,32 +1,29 @@
 import _ from 'lodash';
 
 const compareData = (data1, data2) => {
-  const uniqKeys = _.union(Object.keys(data1), Object.keys(data2));
-  const sortedKeys = _.sortBy(uniqKeys);
+  const sortedKeys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+
   return sortedKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+
     if (!Object.hasOwn(data1, key)) {
-      return { key, type: 'added', value: data2[key] };
+      return { key, type: 'added', value: value2 };
     }
 
     if (!Object.hasOwn(data2, key)) {
-      // Ключ есть только в data1
-      return { key, type: 'deleted', value1: data1[key], value2: undefined };
+      return { key, type: 'deleted', value: value1 };
     }
 
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { key, type: 'nested', children: compareData(data1[key], data2[key]) };
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      return { key, type: 'nested', children: compareData(value1, value2) };
     }
 
-    if (_.isEqual(data1[key], data2[key])) {
-      return { key, type: 'same', value: data1[key] };
+    if (_.isEqual(value1, value2)) {
+      return { key, type: 'same', value: value1 };
     }
 
-    return {
-      key,
-      type: 'updated',
-      value1: data1[key],
-      value2: data2[key],
-    };
+    return { key, type: 'updated', value1, value2 };
   });
 };
 
